bcc502f1654a38436e2174de58a76fa1bc47cb9e
Pedro Lugon <pedromotta20@gmail.com>
2025-11-30 23:54:34 -0300

    fix: remove unused import

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Pokemon, PokemonService } from '../../services/pokemon.service';
 import { CommonModule } from '@angular/common';
 import { catchError, of, tap } from 'rxjs';
